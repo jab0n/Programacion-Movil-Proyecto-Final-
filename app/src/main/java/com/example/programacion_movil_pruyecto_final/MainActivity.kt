b4d87c0fd107c79d0b92b4dffbb977d99fd19d60
unknown <jab0n@outlook.com>
2025-10-07 11:30:16 -0600

    V0.1.1

--- app/src/main/java/com/example/programacion_movil_pruyecto_final/MainActivity.kt
+++ app/src/main/java/com/example/programacion_movil_pruyecto_final/MainActivity.kt
@@ -3,38 +3,37 @@ package com.example.programacion_movil_pruyecto_final
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController // Import the missing class
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.programacion_movil_pruyecto_final.ui.theme.ProgramacionMovilPruyectoFinalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ProgramacionMovilPruyectoFinalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "pantalla1") {
+                composable("pantalla1") { PantallaUno(navController) }
+                composable("pantalla2") { PantallaDos(navController) }
+                composable("pantalla3") { PantallaTres(navController) }
+                composable("pantalla4") { PantallaCuatro(navController) }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!"
     )
 }
 
@@ -43,5 +42,46 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ProgramacionMovilPruyectoFinalTheme {
         Greeting("Android")
+    }
+}
+
+
+@Composable
+fun PantallaUno(navController: NavHostController) {
+    Column {
+        Text("Pantalla Uno")
+        Button(onClick = { navController.navigate("pantalla2") }) {
+            Text("Ir a Pantalla Dos")
+        }
+    }
+}
+
+@Composable
+fun PantallaDos(navController: NavHostController) {
+    Column {
+        Text("Pantalla Dos")
+        Button(onClick = { navController.navigate("pantalla3") }) {
+            Text("Ir a Pantalla Tres")
+        }
+    }
+}
+
+@Composable
+fun PantallaTres(navController: NavHostController) {
+    Column {
+        Text("Pantalla Tres")
+        Button(onClick = { navController.navigate("pantalla4") }) {
+            Text("Ir a Pantalla Cuatro")
+        }
+    }
+}
+
+@Composable
+fun PantallaCuatro(navController: NavHostController) {
+    Column {
+        Text("Pantalla Cuatro")
+        Button(onClick = { navController.popBackStack("pantalla1", inclusive = false) }) {
+            Text("Volver a Pantalla Uno")
+        }
     }
 }
